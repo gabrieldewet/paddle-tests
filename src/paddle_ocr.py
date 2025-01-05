12b4db8fd676092d13c14e6339e5d3686fc91934
gabrieldewet <giebels.dewet@gmail.com>
2025-01-05 22:30:04 +0100

    add onnx + rapidocr infrence

---
+++ src/paddle_ocr.py
@@ -0,0 +1,38 @@
+import logging
+
+import numpy as np
+import pymupdf
+from paddleocr import PaddleOCR
+from PIL import Image
+
+logger = logging.getLogger("ppocr")
+logger.setLevel(logging.INFO)
+
+ocr = PaddleOCR(
+    cls_model_dir="./models/ch_ppocr_mobile_v2.0_cls_infer/",
+    det_model_dir="./models/en_PP-OCRv3_det_infer/",
+    rec_model_dir="./models/en_PP-OCRv3_rec_infer/",
+    use_angle_cls=True,
+    use_gpu=True,
+    use_onnx=False,
+    lang="fr",
+)
+
+
+def ocr_pdf(page, model=ocr):
+    mat = pymupdf.Matrix(2, 2)
+    pm = page.get_pixmap(matrix=mat, alpha=False)
+    img = Image.frombytes("RGB", [pm.width, pm.height], pm.samples)
+
+    return model.ocr(np.array(img), cls=False)
+
+
+if __name__ == "__main__":
+    pdf_path = "data/test_pdf.pdf"
+    pdf = pymupdf.open(pdf_path)
+
+    for page in pdf:
+        result = ocr_pdf(page)
+        print(result[0][0])
+
+        break
---
+++ src/rapid_ocr.py
@@ -0,0 +1,33 @@
+import logging
+
+import numpy as np
+import pymupdf
+from PIL import Image
+from rapidocr_onnxruntime import RapidOCR
+
+model = RapidOCR(
+    # cls_model_path="./models/cls_onnx/model.onnx",
+    # det_model_path="./models/det_onnx/model.onnx",
+    # rec_model_path="./models/rec_onnx/model.onnx",
+)
+
+
+def ocr_pdf(page, model=model):
+    mat = pymupdf.Matrix(2, 2)
+    pm = page.get_pixmap(matrix=mat, alpha=False)
+    img = Image.frombytes("RGB", [pm.width, pm.height], pm.samples)
+
+    return model(np.array(img))
+
+
+if __name__ == "__main__":
+    pdf_path = "data/test_pdf.pdf"
+    pdf = pymupdf.open(pdf_path)
+
+    for page in pdf:
+        result, elapse = ocr_pdf(page)
+        # print(result)
+        print(elapse)
+        print(result[0])
+
+        break
